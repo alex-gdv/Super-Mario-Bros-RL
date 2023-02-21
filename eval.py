1f4c51ab5067195822abd70bfc8dcb8716afbad1
alex-gdv <alex-gdv@proton.me>
2023-02-21 16:35:15 +0000

    made more changes

--- eval.py
+++ eval.py
@@ -1,59 +1,72 @@
 import pandas as pd
 import gym_super_mario_bros
-import torch
 import time
 from nes_py.wrappers import JoypadSpace
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+import math
+from stable_baselines3 import PPO, DQN
 
 from wrappers import *
 
 WORLDS = [1, 2, 3, 4, 5, 6, 7, 8]
 STAGES = [1, 2, 3, 4]
 
-def arrange(s):
-    if not type(s) == "numpy.ndarray":
-        s = np.array(s)
-    assert len(s.shape) == 3
-    ret = np.transpose(s, (2, 0, 1))
-    return np.expand_dims(ret, 0)
+def mean_std(lst):
+    avg = sum(lst) / len(lst)
+    std = math.sqrt(sum([(num-avg)**2 for num in lst])/len(lst))
+    return avg, std
 
 # model_name follows the rule {algorithm}_{SMB version}_{movement type}
-def eval_smb_model(model, model_name, smb_version="v0", movement=SIMPLE_MOVEMENT, results_path="./results/"):
+def eval_smb_model(algorithm, model_name, smb_version="v0", movement=SIMPLE_MOVEMENT, results_path="./results/", model_path="./models/"):
     results = []
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model.to(device)
-    model.eval()
+    model = algorithm.load(model_path + model_name)
     for world in WORLDS:
         for stage in STAGES:
+            print(f"World {world} Stage {stage}")
+            N = 5
+            rewards = []
+            distances = []
+            completion_rate = 0
+
             env = gym_super_mario_bros.make(f"SuperMarioBros-{world}-{stage}-{smb_version}")
             env = JoypadSpace(env, movement)
             env = wrap_mario(env)
 
-            total_reward = 0.0
-            done = False
-            s = arrange(env.reset())
-            while not done:
-                env.render()
-                if device == 'cpu':
-                    a = np.argmax(model(s).detach().numpy())
-                else:
-                    a = np.argmax(model(s).cpu().detach().numpy())
-                s_prime, r, done, info = env.step(a)
-                s_prime = arrange(s_prime)
-                total_reward += r
-                s = s_prime
-                time.sleep(0.001)
-            
-            results.append([f"{world}-{stage}", total_reward, info["x_pos"], info["flag_get"]])
+            for _ in range(N):
+                total_reward = 0.0
+                done = False
+                s = env.reset()
+                while not done:
+                    env.render()
+                    a, _ = model.predict(s)
+                    print(type(a))
+                    print(a)
+                    s_prime, r, done, info = env.step(a)
+                    total_reward += r
+                    s = s_prime
+                    time.sleep(0.001)
+                
+                rewards.append(total_reward)
+                distances.append(info["x_pos"])
+                completion_rate += info["flag_get"]
+
+            reward_avg, reward_std = mean_std(rewards)
+            distance_avg, distance_std = mean_std(distances)
+            completion_rate /= N
+
+            results.append([f"{world}-{stage}", reward_avg, reward_std, distance_avg, distance_std, completion_rate])
             env.close()
             
-    results = pd.DataFrame(data=results, columns=["Level", "Reward", "Distance", "Completed"]).set_index("Level")
+    results = pd.DataFrame(data=results, columns=["Level", "Reward Avg", "Reward Std", "Distance Avg", "Distance Std", "Completion Rate"]).set_index("Level")
     results.to_csv(path_or_buf=f"{results_path}{model_name}.csv")
     
-    averages = results[["Reward", "Distance"]].mean()
-    stds = results[["Reward", "Distance"]].std()
-    completed = results["Completed"].sum()
+    averages = results[["Reward Avg", "Distance Avg"]].mean()
+    stds = results[["Reward Avg", "Distance Avg"]].std()
+    completed = results["Completion Rate"].sum()
     file = open(f"{results_path}/averages.csv", "a")
     # Model Name, Reward Mean, Reward Std, Distance Mean, Distance Std, Number Of Levels Completed
-    file.write(f"{model_name},{averages['Reward']},{stds['Reward']},{averages['Distance']},{stds['Distance']},{completed}")
+    file.write(f"{model_name},{averages['Reward Avg']},{stds['Reward Avg']},{averages['Distance Avg']},{stds['Distance Avg']},{completed}")
     file.close()
+
+if __name__ == "__main__":
+    eval_smb_model(DQN, "dqn_v3_simple_400000", smb_version="v3")
--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from stable_baselines3 import PPO, DQN
 from stable_baselines3.common.vec_env.subproc_vec_env import SubprocVecEnv
+from stable_baselines3.common.vec_env.dummy_vec_env import DummyVecEnv
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from nes_py.wrappers import JoypadSpace
 import sys
+import gym
 
 from wrappers import wrap_mario
 from callback import SMB_Callback
 
-NPROC = 1
+DAYTIME_CASTLE_STAGES = ["1-1", "1-3", "2-1", "2-3", "4-1", "5-1", "5-2", "5-3", "7-1", "7-3", "8-1", "8-2", "8-3"]
+NPROC = 4
 
-def make_env(seed, smb_version="v3", movement = SIMPLE_MOVEMENT):
+def make_env(seed=None, smb_version="v0", movement=SIMPLE_MOVEMENT):
     def _f():
+        env = gym.make(f"SuperMarioBrosRandomStages-{smb_version}", stages=DAYTIME_CASTLE_STAGES)
         env = gym_super_mario_bros.make(f"SuperMarioBrosRandomStages-{smb_version}")
         env = JoypadSpace(env, movement)
         env = wrap_mario(env)
-        env.seed(seed)
+        if seed is not None:
+            env.seed(seed)
         return env
     return _f
 
@@ -33,10 +38,24 @@ def train_PPO(**kwargs):
     model = PPO("CnnPolicy", env, learning_rate=1e-6, n_steps=n_steps, batch_size=32, verbose=1, tensorboard_log="./logs/")
     model.learn(total_timesteps=n_steps*NPROC*1000, callback=callback)
 
+def test(algorithm, model_path, **kwargs):
+    env = DummyVecEnv([make_env(**kwargs)])
+    model = algorithm.load(model_path, env)
+    for _ in range(10):
+        obs = env.reset()
+        while True:
+            env.render()
+            action, _ = model.predict(obs)
+            obs, _, done, _ = env.step(action)
+            if done:
+                obs = env.reset()
+                break
+
 if __name__ == "__main__":
     args_count = len(sys.argv)
-    if args_count == 2:
-        if sys.argv[1] == "dqn":
-            train_DQN()
-        else:
-            train_PPO()
+    # if args_count == 2:
+    #     if sys.argv[1] == "dqn":
+    #         train_DQN()
+    #     else:
+    #         train_PPO()
+    test(DQN, "./models/dqn_v3_simple_400000")
